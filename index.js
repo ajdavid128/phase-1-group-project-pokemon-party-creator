abdb2d6a5b83b85a85495c424e23d2b7fff81374
Aaron David <ajdavid128@gmail.com>
2022-10-24 15:24:49 -0500

    made fetch to grab sprites, made function to display selected sprites with names

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 //Global variables
 const select = document.querySelector('#generations-list');
+const partyContainer = document.querySelector('#selected-pokemon');
+const form = document.querySelector('#generations-form');
 
+const submitButton = document.createElement('button');
+submitButton.innerText = 'I Choose You!'
 
 //Get generation names
 function getGenOneNames(){
     fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
     .then(response => response.json())
     .then(data => renderNames(data))
+}
+
+function fetchSprites(name){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+    .then(res => res.json())
+    .then(data => (renderSprites(data)));
+}
+
+function renderSprites(eachSprite){
+    const spriteImage = document.createElement('img');
+    const spriteName = document.createElement('h3');
+
+    spriteImage.src = eachSprite.sprites.front_default;
+    spriteName.textContent = eachSprite.name;
+
+    partyContainer.append(spriteImage)
+    partyContainer.append(spriteName)
 }
 
 //Render the generation names
@@ -16,17 +37,58 @@ function renderNames(oneName){
     const blankSpace = document.createElement('option')
     blankSpace.textContent = '';
     select.appendChild(blankSpace);
+    
 
     //Iterate through the generation names and append them to the dropdown
     oneName.results.forEach(element => {
     
         const options = document.createElement("option");
-        options.textContent = element.name
-        options.value = element.name
+        // let pokemonNumber = 0
+        // for(let i = pokemonNumber; i < 152; i++){
+        //     // ++pokemonNumber
+        //     options.textContent = `${i} ${element.name}`
+        // }
+        options.textContent = element.name;
+        options.value = element.name;
+        options.classList = 'dropdown-value'
+    
         select.appendChild(options);
+        }) 
 
-        })    
+    form.append(submitButton);   
 }
+
+//Submit Button Listener
+submitButton.addEventListener('click', (e) => {
+    console.log(e.type)
+    // 
+    let submitCounter = 0
+        if(submitCounter < 6){
+            ++submitCounter
+            submitButton.style.display = 'block';
+        } else {
+            
+        }
+})
+submitButton.style.display = 'none';
+
+
+//Form Submission
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    fetchSprites(e.target['generations-list'].value);
+    // console.log(e.target['generations-list'].value)
+})
+
+
+
+
+
 
 //invoke function
 getGenOneNames()
+
+
+// function selectedPartyPokemon(eachPokemon){
+
+// }
