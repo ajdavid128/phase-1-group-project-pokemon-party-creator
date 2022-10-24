6c4b3d9302e961c3fc4417eec9da574fde58daa4
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-24 13:35:26 -0400

    Assigned the first dropdown to blank space. Added comments.

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const select = document.querySelector('#generations-list')
+//Global variables
+const select = document.querySelector('#generations-list');
 
+
+//Get generation names
 function getGenOneNames(){
     fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
     .then(response => response.json())
     .then(data => renderNames(data))
 }
-getGenOneNames()
 
+//Render the generation names
 function renderNames(oneName){
+    
+    //Assign the first dropdown to blank and append it
+    const blankSpace = document.createElement('option')
+    blankSpace.textContent = '';
+    select.appendChild(blankSpace);
+
+    //Iterate through the generation names and append them to the dropdown
     oneName.results.forEach(element => {
-        const options = document.createElement("option")
+    
+        const options = document.createElement("option");
         options.textContent = element.name
         options.value = element.name
-        select.appendChild(options)
+        select.appendChild(options);
 
-        })
-   
+        })    
 }
+
+//invoke function
+getGenOneNames()
