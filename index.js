6d9f974907cf27ab8618e4a3c34db58adca3616f
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-25 13:00:31 -0400

    Made all sprite images to be displayed when generate button is clicked. Made the images click event, and put the party container

--- index.js
+++ index.js
@@ -1,24 +1,74 @@
 //Global variables
 const select = document.querySelector('#generations-list');
 const partyContainer = document.querySelector('#selected-pokemon');
-const form = document.querySelector('#generations-form');
-const submitButton = document.createElement('button');
 const removeBtn = document.querySelector('#removeButton');
 const partyDisplayText = document.querySelector('h2');
 const pokemonButton = document.createElement('button');
-let submitCounter = 0;
-let namesCounter = 0;
+const pokemonContainer = document.querySelector('#pokemon-container');
+const header = document.querySelector('header');
 
+
+partyDisplayText.style.display = 'none';
 pokemonButton.textContent = "Generate Pokemon";
-document.body.appendChild(pokemonButton);
+header.appendChild(pokemonButton);
 
-//Get generation names
-function getGenOneNames(){
+
+
+
     
-    fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
-    .then(response => response.json())
-    .then(data => renderNames(data))
-}
+pokemonButton.addEventListener('click', () => {
+
+    
+    
+        fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
+        .then(response => response.json())
+        .then(data => renderNames(data))
+
+    function renderNames(oneName){
+    
+        oneName.results.forEach(element => {
+   
+           
+       const name = document.createElement('p');
+   
+          name.innerText = element.name
+          fetchPokemonNames(name.innerText);
+   
+   
+   
+       })
+   }
+   
+   function fetchPokemonNames(name) {
+   
+       
+       fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+       .then(resp => resp.json())
+       .then(data => {
+
+            const img = document.createElement('img');
+
+           img.src = data.sprites.front_default;
+
+           img.addEventListener('click', (e) => {
+
+             fetchSprites(name);
+
+
+           })
+
+          pokemonContainer.appendChild(img);
+          
+
+            
+       })
+   
+   }
+
+})
+
+
+
 
 //Get sprites info
 function fetchSprites(name){
@@ -27,6 +77,8 @@ function fetchSprites(name){
     .then(res => res.json())
     .then(data => (renderSprites(data)));
 }
+
+
 
 //Render sprites
 function renderSprites(eachSprite){
@@ -40,16 +92,6 @@ function renderSprites(eachSprite){
 
     spriteImage.src = eachSprite.sprites.front_default;
     spriteName.textContent = eachSprite.name;
-
-    //Render Sprites to page
-    pokemonButton.addEventListener('click', () => {
-        // fetch(`https://pokeapi.co/api/v2/pokemon/${options.value}/`)
-        //         .then(res => res.json())
-        //         .then(data => {
-                    const img = document.createElement('img');
-                    img.src = eachSprite.sprites.front_default;
-                    document.body.appendChild(img);
-    })
 
 
 
@@ -89,105 +131,4 @@ function renderSprites(eachSprite){
     cards.append(spriteName, spriteImage, removeButton);
     partyContainer.appendChild(cards);
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Making the partyDisplayText disappear till submission
-// partyDisplayText.style.display = 'none';
-
-// //Form Submission
-// form.addEventListener('submit', (e) =>{
-
-//     e.preventDefault();
-//     partyDisplayText.style.display = 'block'
-//     fetchSprites(e.target['generations-list'].value);
-//     form.reset();
-  
-// })
-
-// //Assigning a value to the submit button
-// submitButton.innerText = 'I Choose You!'
-
-// //Submit button even listener
-// submitButton.addEventListener('click', (e) => {
-
-//     if(e.type) {
-
-//         ++submitCounter;
-        
-//         if(submitCounter === 6){
-    
-//             submitButton.style.display = 'none'        
-    
-//         }
-//     }
-
-// });
-
-
-// submitButton.classList = 'submit-button';
-
-
-
-
-//invoke function
-getGenOneNames();
-
-
-
-//Render the generation names
-function renderNames(oneName){
-    
-    // //Assign the first dropdown to blank and append it
-    // const blankSpace = document.createElement('option')
-    // blankSpace.textContent = '';
-    // select.appendChild(blankSpace);
-
-
-    //Iterate through the generation names and append them to the dropdown
-    oneName.results.forEach(element => {
-
-    
-        // const options = document.createElement("option");
-
-        // //Assign a number to each name
-        // if(namesCounter <= 151) {
-
-        //     namesCounter++;
-        //     options.innerText = `${namesCounter}. ${element.name}`
-           
-        // }
-        
-        //     options.value = element.name;
-        //     options.classList = 'dropdown-value'
-    
-        //     select.appendChild(options);
-
-            // select.addEventListener('mouseover', () => {
-            //      fetch(`https://pokeapi.co/api/v2/pokemon/${options.value}/`)
-            //     .then(res => res.json())
-            //     .then(data => {
-            //         const img = document.createElement('img');
-            //         img.src = data.sprites.front_default;
-            //         document.body.appendChild(img);
-
-            //         console.log(data);
-            //     });
-                
-            // })
-        }) 
-
-    // form.append(submitButton);   
 }
