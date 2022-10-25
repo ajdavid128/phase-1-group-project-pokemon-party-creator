f849afc3372b1580157bdc09d86dcf4e783f7541
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-24 22:25:45 -0400

    Assigned a number to each name in the dropdown.

--- index.js
+++ index.js
@@ -2,58 +2,18 @@
 const select = document.querySelector('#generations-list');
 const partyContainer = document.querySelector('#selected-pokemon');
 const form = document.querySelector('#generations-form');
-let submitCounter = 0;
-
 const submitButton = document.createElement('button');
-submitButton.innerText = 'I Choose You!'
+const removeBtn = document.querySelector('#removeButton');
+let submitCounter = 0;
+let namesCounter = 0;
+
 
 //Get generation names
 function getGenOneNames(){
+    
     fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
     .then(response => response.json())
     .then(data => renderNames(data))
-}
-
-function fetchSprites(name){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
-    .then(res => res.json())
-    .then(data => (renderSprites(data)));
-}
-
-function renderSprites(eachSprite){
-    const spriteImage = document.createElement('img');
-    const spriteName = document.createElement('h3');
-    const removeButton = document.createElement('button');
-
-    spriteImage.src = eachSprite.sprites.front_default;
-    spriteName.textContent = eachSprite.name;
-    removeButton.textContent = 'Return';
-    removeButton.classList = 'removeButton';
-
-    function removeFunction() {
-
-        removeButton.addEventListener('click', (e) => {
-
-            let removeCounter = 0;
-
-            if(e.type) {
-
-                removeCounter++;
-            }
-
-            spriteImage.remove()
-            spriteName.remove()
-            removeButton.remove()
-        })
-
-    }
-    
-
- 
-    partyContainer.append(spriteName, spriteImage, removeButton);
-    // partyContainer.append(spriteName)
-    
-    
 }
 
 //Render the generation names
@@ -63,14 +23,21 @@ function renderNames(oneName){
     const blankSpace = document.createElement('option')
     blankSpace.textContent = '';
     select.appendChild(blankSpace);
-    
+
 
     //Iterate through the generation names and append them to the dropdown
     oneName.results.forEach(element => {
     
         const options = document.createElement("option");
 
-        options.textContent = element.name;
+        //Assign a number to each name
+        if(namesCounter <= 151) {
+
+            namesCounter++;
+            options.innerText = `${namesCounter}. ${element.name}`
+           
+        }
+        
         options.value = element.name;
         options.classList = 'dropdown-value'
     
@@ -80,45 +47,69 @@ function renderNames(oneName){
     form.append(submitButton);   
 }
 
+//Get sprites info
+function fetchSprites(name){
 
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+    .then(res => res.json())
+    .then(data => (renderSprites(data)));
+}
 
-submitButton.addEventListener('click', (e) => {
+//Render sprites
+function renderSprites(eachSprite){
 
-    if(e.type) {
-        ++submitCounter;
+    const spriteImage = document.createElement('img');
+    const spriteName = document.createElement('h3');
+    const removeButton = document.createElement('button');
+
+    spriteImage.src = eachSprite.sprites.front_default;
+    spriteName.textContent = eachSprite.name;
+    removeButton.textContent = 'Return';
+    removeButton.classList = 'removeButton';
+
+    removeButton.addEventListener('click', (e) => {
+
+        spriteImage.remove()
+        spriteName.remove()
+        removeButton.remove()        
         
-        if(submitCounter === 6){
-            submitButton.style.display = 'none'
-        }
-    }
-    // add removal and set submitCounter--
-        
-})
+    })
+           
+    partyContainer.append(spriteName, spriteImage, removeButton);
+
+}
 
 //Form Submission
 form.addEventListener('submit', (e) =>{
+
     e.preventDefault();
     fetchSprites(e.target['generations-list'].value);
-    // console.log(e.target['generations-list'].value)
+    form.reset();
+  
 })
 
+//Assigning a value to the submit button
+submitButton.innerText = 'I Choose You!'
 
+//Submit button even listener
+submitButton.addEventListener('click', (e) => {
 
+    if(e.type) {
 
+        ++submitCounter;
+        
+        if(submitCounter === 6){
+    
+            submitButton.style.display = 'none'        
+    
+        }
+    }
+
+});
 
 
 //invoke function
-getGenOneNames()
-
-
-
-// submitCounter ++ = 6 == dispapears
-// removeCounter ++ 3 
-
-
-// submitButton - removeButton = 3
-
-// 3 = how many sumbit could be hit before disappearing 
+getGenOneNames();
 
 
 
