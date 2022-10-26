9ab5db19ca36c54bcd12693041283c7561e5dea5
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-26 09:44:36 -0400

    Added a mouse over, added inline css to hide elements on page. moved the remove button to the submit division.

--- index.js
+++ index.js
@@ -9,40 +9,29 @@ const header = document.querySelector('header');
 const form = document.querySelector('form');
 const submitContainer = document.querySelector('#submitted-form');
 const formButton = document.querySelector('#submit-party');
+const infoContainer = document.querySelector('#info-container');
+const formArray = [];
+const cardsCounter = [];
 
 
-
-
-//FIX THE RETURN BUTTON ISSUE WITH SUBMIT
-
-
-
-
-
-
-const array = [];
-
-submitContainer.style.display = 'none'
-
-//Counter for party clicks:
-let selectPartyCounter = 0;
-
-
-//Generate Pokemon Button
+// CSS styling & appending
+infoContainer.style.display = 'none';
+formButton.style.display = 'none';
 partyDisplayText.style.display = 'none';
 pokemonButton.textContent = "Generate Pokemon";
+pokemonButton.style.margin = '0 auto';
+pokemonButton.style.display = 'block';
 header.appendChild(pokemonButton);
 
 
 //Click Event -> Render Sprites to Page
 pokemonButton.addEventListener('click', () => {
     
-
+    pokemonButton.style.display = 'none';
     //Fetch to grab 151 Pokemon
     fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
         renderNames(data);
     })
 
@@ -68,13 +57,18 @@ pokemonButton.addEventListener('click', () => {
                 
                 //Click to select sprites and append to "Party Container"
                 img.addEventListener('click', (e) => {
+                    
+                    formButton.style.margin = '0 auto';
+
                     if(partyContainer.children.length < 6){
                         fetchSprites(name);       
                     }
+
                 }) 
 
             pokemonContainer.appendChild(img);    
        })
+       
    }
 })
 
@@ -96,33 +90,11 @@ function renderSprites(eachSprite){
     const cards = document.createElement('div');
     const spriteImage = document.createElement('img');
     const spriteName = document.createElement('h3');
-    const removeButton = document.createElement('button');
-   
     cards.classList = 'card';
-
+    
+        
     spriteImage.src = eachSprite.sprites.front_default;
     spriteName.textContent = eachSprite.name;
-
-
-
-    // Design for remove button
-    removeButton.textContent = 'Return';
-    removeButton.classList = 'removeButton';
-    removeButton.style.backgroundColor = "#3B4CCA";
-    removeButton.style.display = 'inline-block';
-    removeButton.style.padding = ' 0.3em 1.2em';
-    removeButton.style.margin = '0 0.1em 0.1em 0';
-    removeButton.style.border = "0.16em solid rgba(255,255,255,0)";
-    removeButton.style.borderRadius = '2em';
-    removeButton.style.boxSizing = 'border-box';
-    removeButton.style.textDecoration = 'none';
-    removeButton.style.fontFamily = 'Roboto, sans-serif';
-    removeButton.style.fontWeight = '500';
-    removeButton.style.color = '#FFFFFF';
-    removeButton.style.textShadow = '0 0.04em 0.04em rgba(0,0,0,0.35)';
-    removeButton.style.textAlign = 'center';
-    removeButton.style.transition = 'all 0.2s';
-
 
    
     spriteImage.style.display = 'block';
@@ -130,40 +102,86 @@ function renderSprites(eachSprite){
     spriteImage.style.marginRight = 'auto';
     spriteImage.style.width = '50';
 
-    removeButton.addEventListener('click', (e) => {
-
-        spriteImage.remove()
-        spriteName.remove()
-        removeButton.remove()        
-        cards.remove();
-    })
+    
          
-    array.push(cards);
-    cards.append(spriteName, spriteImage, removeButton);
+    formArray.push(cards);
+    cards.append(spriteName, spriteImage);
     partyContainer.appendChild(cards);
+    
+    cardsCounter.push(cards);
+    if(cardsCounter.length >= 1) {
+
+        formButton.style.display = 'block';
+        
+    }
 
 }
-console.log(array);
-
 
 
 //submit form stuff
-
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
-    array.forEach(element => {
+    formArray.forEach(element => {
         const submitDiv = document.createElement('div');
         const arrayItemText = document.createElement('h4');
         const arrayItemImg = document.createElement('img');
+        const removeButton = document.createElement('button');
+
+        // Design for remove button
+        removeButton.textContent = 'Remove';
+        removeButton.classList = 'removeButton';
+        removeButton.style.backgroundColor = "#3B4CCA";
+        removeButton.style.display = 'inline-block';
+        removeButton.style.padding = ' 0.3em 1.2em';
+        removeButton.style.margin = '0 0.1em 0.1em 0';
+        removeButton.style.border = "0.16em solid rgba(255,255,255,0)";
+        removeButton.style.borderRadius = '2em';
+        removeButton.style.boxSizing = 'border-box';
+        removeButton.style.textDecoration = 'none';
+        removeButton.style.fontFamily = 'Roboto, sans-serif';
+        removeButton.style.fontWeight = '500';
+        removeButton.style.color = '#FFFFFF';
+        removeButton.style.textShadow = '0 0.04em 0.04em rgba(0,0,0,0.35)';
+        removeButton.style.textAlign = 'center';
+        removeButton.style.transition = 'all 0.2s';
+
+        removeButton.addEventListener('click', (e) => {
+
+            submitDiv.remove();
+      
+           
+        })
 
         arrayItemText.innerText = element.childNodes[0].innerText
         arrayItemImg.src = element.childNodes[1].src
         submitDiv.classList = 'submitDiv'
 
-        submitDiv.append(arrayItemText, arrayItemImg);
+        arrayItemImg.style.display = 'block';
+        arrayItemImg.style.marginLeft = 'auto';
+        arrayItemImg.style.marginRight = 'auto';
+        arrayItemImg.style.width = '50';
+
+        submitDiv.append(arrayItemText, arrayItemImg, removeButton);
         submitContainer.append(submitDiv)
 
+        const infoImg = document.createElement('img');
+        submitDiv.addEventListener('mouseover', () => {
+            
+            infoContainer.style.display = 'block';
+            
+        })
+
+        submitDiv.addEventListener('mouseout', () => {
+            
+            infoContainer.style.display = 'none';
+            
+        })
+
+        
     });
+
+    formButton.remove(); 
+
     submitContainer.style.display = 'flex';
     partyContainer.style.display = 'none';
 })
