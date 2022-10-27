bb762a0669260c1f68b3b17f186d9c7a3f170185
Aaron David <ajdavid128@gmail.com>
2022-10-27 16:04:12 -0500

    FINAL COMMIT, final touches on styling text, colors, padding, magins, etc., refactored and cleaned up unused code

---
+++ README.md
@@ -0,0 +1,31 @@
+## Pokémon Party Creator
+
+Welcome to Pokémon Party Creator! 
+
+Here we have created a fun little experience for every Pokémon lover out there. In this simple application you can view and select your Pokémon dream team. Please feel free to read through the directions below on how to navigate this webpage:
+
+Step #1: 
+Select the “Generate Pokémon'' button. This will display all 151 Pokémon from the first generation. 
+
+Step #2:  
+Look through all the Pokémon sprites and pick your 6 favorites. When you select a Pokémon it will jump into your “Pokémon Party” at the top of the page. 
+
+Step #3: 
+When you have selected your 6 Pokémon hit the submit button to confirm your party.
+
+Step #4: 
+Hover the Pokéball cursor over the Pokémon in your party and an information box will appear containing descriptions about each member.
+
+Step #5: 
+Do it all again! At the bottom of the page there will be a “Pick New Party” button that will bring you back to Step #1.
+
+Thanks for stopping by and have fun! 
+
+## Project Details
+This single-page web application was designed by Aaron David, Nick Phelps and Jonny Tilahun for their Phase 1 group project at Flatiron School during the 10-10-22 East Cohort.
+
+The focus of this project was to use a GET .fetch() request on a public API. For the project we decided to use the Poké API (https://pokeapi.co/docs/v2). This API did not require an Authentication Key and had a significant amount of End Points to pursue.
+
+We chose the Poké API as a fun homage to our childhood hobbies. 
+
+The webpage was built from the ground up using HMTL, JavaScript and CSS. 
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//Global variables
+//GLOBAL VARIABLES
+
+//QUERY SELECTION
 const select = document.querySelector('#generations-list');
 const partyContainer = document.querySelector('#selected-pokemon');
 const removeBtn = document.querySelector('#refreshButton');
-// const partyDisplayText = document.querySelector('h2');
-const pokemonButton = document.createElement('button');
 const pokemonContainer = document.querySelector('#pokemon-container');
 const header = document.querySelector('header');
 const form = document.querySelector('form');
@@ -12,64 +12,51 @@ const formButton = document.querySelector('#submit-party');
 const infoContainer = document.querySelector('#info-container');
 const refreshButton = document.querySelector('#refresh-page');
 const descriptionDiv = document.querySelector('#description');
+
+//CREATED ELEMENTS
+const pokemonButton = document.createElement('button');
 const descriptionP = document.createElement('p');
-descriptionP.id = "description-p"
 
-
-
-const tempStorage = document.querySelector('#temp-storage');
-
-const descriptionStorage = document.createElement('div');
-descriptionStorage.id = 'description-storage';
-tempStorage.appendChild(descriptionStorage);
-
-
+//EMPTY ARRAYS
 const formArray = [];
 const cardsCounter = [];
 const infoArray = [];
 
-// Button Attributes
-pokemonButton.id = 'gen-btn'
-formButton.id = 'form-btn'
-refreshButton.id = 'refresh-btn'
+//BUTTON & TAG Attributes
+pokemonButton.id = 'gen-btn';
+formButton.id = 'form-btn';
+refreshButton.id = 'refresh-btn';
+descriptionP.id = "description-p";
 
-
-// CSS styling & appending
+// CSS STYLING & APPENDING
 refreshButton.style.display = 'none';
 infoContainer.style.display = 'none';
 formButton.style.display = 'none';
-// partyDisplayText.style.display = 'none';
 pokemonButton.textContent = "Generate Pokemon";
 pokemonButton.style.margin = '0 auto';
 pokemonButton.style.display = 'block';
+
 header.appendChild(pokemonButton);
-
-tempStorage.style.display = 'none';
-
 
 //Click Event -> Render Sprites to Page
 pokemonButton.addEventListener('click', () => {
-    
     pokemonButton.style.display = 'none';
     descriptionDiv.style.display = 'none';
+
     //Fetch to grab 151 Pokemon
     fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
     .then(response => response.json())
     .then(data => {
         renderNames(data);
-    })
+    });
 
     function renderNames(oneName){
-
         oneName.results.forEach(element => {    
         const name = document.createElement('p');
-        name.innerText = element.name
-
+        name.innerText = element.name;
         fetchPokemonNames(name.innerText);
-    
-        
-       })
-   }
+       });
+   };
    
    function fetchPokemonNames(name) {
 
@@ -80,49 +67,36 @@ pokemonButton.addEventListener('click', () => {
 
             const img = document.createElement('img');
             img.src = data.sprites.front_default;
-                
+
                 //Click to select sprites and append to "Party Container"
                 img.addEventListener('click', (e) => {
-                    
                     formButton.style.margin = '0 auto';
-
                     if(partyContainer.children.length < 6){
                         fetchSprites(name);       
-                    }
-
-                }) 
-                
+                    };
+                });  
             pokemonContainer.appendChild(img);    
-       })
-       
-   }
-})
-
-
-
+       });
+   };
+});
 
 //Get sprites info
 function fetchSprites(name){
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
     .then(res => res.json())
     .then(data => {
-
-        renderSprites(data)
-        // console.log(data)
+        renderSprites(data);
     });
-}
-
-
+};
 
 //Render sprites
 function renderSprites(eachSprite){
     
     const card = document.createElement('div');
 
-    card.setAttribute('pokemonName', eachSprite.name)
+    card.setAttribute('pokemonName', eachSprite.name);
     const spriteImage = document.createElement('img');
 
-    // console.log(card)
     const spriteName = document.createElement('h3');
     card.classList = 'card';
         
@@ -140,13 +114,9 @@ function renderSprites(eachSprite){
     
     cardsCounter.push(card);
     if(cardsCounter.length >= 1) {
-
-        formButton.style.display = 'block';
-        
-    }
-
-}
-
+        formButton.style.display = 'block';   
+    };
+};
 
 //submit form stuff
 form.addEventListener('submit', (e) =>{
@@ -154,18 +124,16 @@ form.addEventListener('submit', (e) =>{
     formArray.forEach(element => {
         const submitDiv = document.createElement('div');
 
-        let name = element.getAttribute('pokemonname')
-
+        let name = element.getAttribute('pokemonname');
        
-        submitDiv.setAttribute('pokemonName', name)
+        submitDiv.setAttribute('pokemonName', name);
 
         const arrayItemText = document.createElement('h3');
         const arrayItemImg = document.createElement('img');
-        
 
-        arrayItemText.innerText = element.childNodes[0].innerText
-        arrayItemImg.src = element.childNodes[1].src
-        submitDiv.classList = 'submitDiv'
+        arrayItemText.innerText = element.childNodes[0].innerText;
+        arrayItemImg.src = element.childNodes[1].src;
+        submitDiv.classList = 'submitDiv';
 
         arrayItemImg.style.display = 'block';
         arrayItemImg.style.marginLeft = 'auto';
@@ -173,48 +141,27 @@ form.addEventListener('submit', (e) =>{
         arrayItemImg.style.width = '50';
 
         submitDiv.append(arrayItemText, arrayItemImg);
-        submitContainer.append(submitDiv)
+        submitContainer.append(submitDiv);
 
         const infoImg = document.createElement('img');
+
         submitDiv.addEventListener('mouseover', (e) => {
-            // console.log(e.currentTarget);
             infoContainer.style.display = 'block';
-
-            // const finalLi = document.createElement('li');
-            // finalLi.textContent = 
-
-            // infoContainer.append(finalLi)
             pokemonContainer.style.display = 'none';
-        })
-
+        });
         infoArray.push(element.childNodes[0].innerText);
-
-        fetchDescription(arrayItemText.innerText)
-    });
-
-    
+        fetchDescription(arrayItemText.innerText);
+    }); 
     formButton.remove(); 
-
-    // partyDisplayText.style.display = 'block';
     submitContainer.style.display = 'flex';
     partyContainer.style.display = 'none';
-    refreshButton.style.display = 'block';
-    
+    refreshButton.style.display = 'block';   
+});
 
-    // partyDisplayText.appendChild(header)
-})
-
-
+//Pick New Pokemon Event
 refreshButton.addEventListener('click', () => {
     location.reload();
-})
-
-
-// infoArray.forEach(element => {
-//     fetchDescription(element);
-// })
-
-
+});
 
 function fetchDescription(name) {
 
@@ -225,43 +172,17 @@ function fetchDescription(name) {
         const descriptli = document.createElement('li');
         const nameSpan = document.createElement('span');
         const descriptionSpan = document.createElement('span');
-        descriptli.id = `${name}`
-        descriptli.classList = 'flavor-text'
+
+        descriptli.id = `${name}`;
+        descriptli.classList = 'flavor-text';
+
         nameSpan.innerText = `${name[0].toUpperCase()}${name.slice(1,name.length)}:`;
         descriptionSpan.innerText = ` ${data.flavor_text_entries[0].flavor_text}`;
-        descriptli.append(nameSpan, descriptionSpan);
 
         nameSpan.style.color = '#ffde00';
         nameSpan.style.textDecoration = 'underline';
 
+        descriptli.append(nameSpan, descriptionSpan);
         infoContainer.appendChild(descriptli);
-
-    })
-    
-}
-// Trying to add styling to first word in flavor text
-// let test = []
-// console.log(test)
-// test.forEach(element => {
-//     element.style.textColor = 'red';
-// })
-
-// descriptionP.textContent = `Welcome to Pokémon Party Creator! 
-
-// Here we have created a fun little experience for every Pokémon lover out there. In this simple application you can view and select your Pokémon dream team. Below you will find directions on how to navigate this webpage:
-
-// Step #1: Select the “Generate Pokémon'' button. 
-// This will display all 151 Pokémon from the first generation. 
-
-// Step #2:  Look through all the Pokémon sprites and pick your 6 favorites. 
-// When you select a Pokemon it will jump into your “Pokémon Party”. 
-
-// Step #3: When you have selected your 6 Pokémon hit the submit button to confirm your party.
-
-// Step #4: Hover the Pokéball cursor over the Pokémon and an information box will appear containing descriptions about each member of your party.
-
-// Step #5: Do it all again! At the bottom of the page there will be a “Pick New Party” button that will bring you back to Step #1.
-
-// Thanks for stopping by and have fun! `;
-
-// descriptionDiv.append(descriptionP);
+    });    
+};
