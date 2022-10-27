38425b83a68607520279d2bf2d4acd209c8cd4a6
Aaron David <ajdavid128@gmail.com>
2022-10-27 11:06:09 -0500

    add main description to HTML, styled text and font, attemtped to style name of each pokemon in info container (no success), tested split() method

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const descriptionDiv = document.querySelector('#description');
 const descriptionP = document.createElement('p');
 descriptionP.id = "description-p"
 
+
+
 const tempStorage = document.querySelector('#temp-storage');
 
 const descriptionStorage = document.createElement('div');
@@ -224,21 +226,53 @@ function fetchDescription(name) {
         descriptli.id = `${name}`
         descriptli.classList = 'flavor-text'
         descriptli.innerText = `${name[0].toUpperCase()}${name.slice(1,name.length)}: ${data.flavor_text_entries[0].flavor_text}`;
-        console.log(descriptli);
+
+        console.log(descriptli.innerText)
+        // console.log(descriptli);
 
         infoContainer.appendChild(descriptli);
 
-        //nidorino, zubat, caterpie's description are in Japanese
+        //nidorino, zubat, caterpie's description are in Japanese/Chinese
         //rattata
 
+        // Grabbing Flavor Text First Word
+        // const flavorTextName = descriptli.innerText.split(' ')[0]
+        // console.log(flavorTextName);
+        // for(let i = 0; i < flavorTextName.length; i++){
+            
+        //         console.log(flavorTextName[i])
+            
+        // }
+        
+        // test.push(flavorTextName)
+        // flavorTextName.style.color = "red";
 
     })
     
 }
+// Trying to add styling to first word in flavor text
+// let test = []
+// console.log(test)
+// test.forEach(element => {
+//     element.style.textColor = 'red';
+// })
 
+// descriptionP.textContent = `Welcome to Pokémon Party Creator! 
 
-descriptionP.textContent = `Welcome to Pokemon Party Creator! Here you can select your dream team to battle through "something". Our team has created a fun little experience for every pokemon lover out there. To begin you will select the "Generate Pokemon" button which will display all 151 Pokemon from the first generation (other generations to come soon). From here feel free to pick your 6 favorite Pokemon, just like in the games! This will become your ideal party. When you've finished selecting your 6 submit. This will allow you to learn more about your party by hovering the pokeball over each party member you selected. If you aren't satisfied with your party, or feel they might not survive the FINAL FOUR, feel free to start again by clicking the "Pick New Party" button and have another go! Thanks for visiting our site!`;
+// Here we have created a fun little experience for every Pokémon lover out there. In this simple application you can view and select your Pokémon dream team. Below you will find directions on how to navigate this webpage:
 
-descriptionDiv.append(descriptionP);
-        
+// Step #1: Select the “Generate Pokémon'' button. 
+// This will display all 151 Pokémon from the first generation. 
 
+// Step #2:  Look through all the Pokémon sprites and pick your 6 favorites. 
+// When you select a Pokemon it will jump into your “Pokémon Party”. 
+
+// Step #3: When you have selected your 6 Pokémon hit the submit button to confirm your party.
+
+// Step #4: Hover the Pokéball cursor over the Pokémon and an information box will appear containing descriptions about each member of your party.
+
+// Step #5: Do it all again! At the bottom of the page there will be a “Pick New Party” button that will bring you back to Step #1.
+
+// Thanks for stopping by and have fun! `;
+
+// descriptionDiv.append(descriptionP);
